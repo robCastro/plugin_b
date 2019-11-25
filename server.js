474879a0bac0c9278bc30ada8f880596b76fed2b
Roberto Castro <roberto.ernesto.castro@gmail.com>
2019-11-25 15:38:12 -0600

    Uso de CORS implementado

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3002;
 
+const cors = require('cors');
+
 app.listen(port);
+app.use(cors());
 
 app.use('/api/pluginb', require('./api/routes/routes'));
+
 console.log('RESTful API server started on: ' + port);
